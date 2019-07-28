e6c02306f1f81bba7cc943a0cf06b285f9877a09
Amit Tripathi <mail2amit19@gmail.com>
2019-07-28 23:29:51 +0200

    Recover when gRPC server is down

--- mdbcli/cli.go
+++ mdbcli/cli.go
@@ -73,6 +73,12 @@ func cli() {
 	var result string
 	log.SetFlags(0)
 	reader := bufio.NewReader(os.Stdin)
+	// Recover if server is down
+	defer func() {
+        if r := recover(); r != nil {
+            fmt.Printf("Error raised while connecting to the server, Error(%s)\n", r)
+        }
+    }()
 
 	for {
 		fmt.Printf("o> ")
